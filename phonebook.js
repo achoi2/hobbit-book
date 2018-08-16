a211e8388756c7bb65f74c9c2552f3303160a188
Andrew <andrewchoi1@hotmail.com>
2018-08-16 14:42:22 -0400

    added post function

--- phonebook.js
+++ phonebook.js
@@ -6,6 +6,16 @@ var writeFile = promisify(fs.writeFile);
 var readFile = promisify(fs.readFile);
 
 var server = http.createServer(function(req, res) {
+    var readbody = function(req, callback) {
+        var body = '';
+        req.on('data', function(chunk) {
+            body += chunk.toString();
+        });
+        req.on('end', function() {
+            callback(body);
+        });
+    };
+
     var contactPrefix = '/contacts/';
     if (req.url === '/contacts' && req.method === 'GET') {
         readFile('hobbit.json').then(function(data) {
@@ -26,23 +36,18 @@ var server = http.createServer(function(req, res) {
             res.end('success');
         });
     } else if (req.url.startsWith(contactPrefix) && req.method === 'POST') {
-        var readbody = function(req, callback) {
-            var body = '';
-            req.on('data', function(chunk) {
-                body += chunk.toString();
-            });
-            req.on('end', function() {
-                callback(body);
-            });
-        };
-
         readbody(req, function(body) {
             var contact = JSON.parse(body);
             readFile('hobbit.json').then(function(data) {
                 var dataJSON = JSON.parse(data.toString());
-                var newArr = dataJSON.push(contact)
-                JSON.stringify(newArr)
+                var newArr = dataJSON.push(contact);
+                JSON.stringify(newArr);
             });
+            res.end('created contact');
+        });
+    } else if (req.url.startsWith(contactPrefix) && req.method === 'PUT') {
+        readbody(req, function(body) {
+            var contact = JSON.parse(body);;
             res.end('created contact');
         });
     }
