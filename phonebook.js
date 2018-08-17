13f11088cb4a8ffdf2aec0c2de8e0c21592d4bad
Andrew <andrewchoi1@hotmail.com>
2018-08-17 16:28:55 -0400

    refactored REST Api

--- phonebook.js
+++ phonebook.js
@@ -5,47 +5,67 @@ const promisify = require('util').promisify;
 const writeFile = promisify(fs.writeFile);
 const readFile = promisify(fs.readFile);
 
-let server = http.createServer((req, res) => {
-    let readbody = (req, callback) => {
-        let body = '';
-        req.on('data', chunk => body += chunk.toString());
-        req.on('end', () => callback(body));
-    };
+let readbody = (req, callback) => {
+    let body = '';
+    req.on('data', chunk => (body += chunk.toString()));
+    req.on('end', () => callback(body));
+};
 
-    let contactPrefix = '/contacts/';
-    
+let getContacts = (req, res) => {
+    readFile('hobbit.json').then(data => {
+        let dataString = data.toString();
+        res.end(dataString);
+    });
+};
+
+let getContact = (req, res) => {
+    readFile('hobbit.json').then(data => {
+        let dataJSON = JSON.parse(data.toString());
+        let name = req.url.slice(contactPrefix.length);
+        res.end('You asked for ' + JSON.stringify(dataJSON[name]));
+    });
+};
+
+let deleteContact = (req, res) => {
+    readFile('hobbit.json').then(data => {
+        let dataJSON = JSON.parse(data.toString());
+        let name = req.url.slice(contactPrefix.length);
+        delete dataJSON[name];
+        res.end('success');
+    });
+};
+
+let postContact = (req, res) => {
+    readbody(req, body => {
+        let contact = JSON.parse(body);
+        let dataJSON = JSON.stringify(contact);
+        writeFile('hobbit.json', dataJSON).then(() => {
+            console.log('success');
+        });
+        res.end('created contact');
+    });
+};
+
+let putContact = (req, res) => {
+    readbody(req, body => {
+        let contact = JSON.parse(body);
+        res.end('created contact');
+    });
+};
+
+let contactPrefix = '/contacts/';
+
+let server = http.createServer((req, res) => {
     if (req.url === '/contacts' && req.method === 'GET') {
-        readFile('hobbit.json').then(data => {
-            let dataString = data.toString();
-            res.end(dataString);
-        });
+        getContacts(req, res);
     } else if (req.url.startsWith(contactPrefix) && req.method === 'GET') {
-        readFile('hobbit.json').then(data => {
-            let dataJSON = JSON.parse(data.toString());
-            let name = req.url.slice(contactPrefix.length);
-            res.end('You asked for ' + JSON.stringify(dataJSON[name]));
-        });
+        getContact(req, res);
     } else if (req.url.startsWith(contactPrefix) && req.method === 'DELETE') {
-        readFile('hobbit.json').then(data => {
-            let dataJSON = JSON.parse(data.toString());
-            let name = req.url.slice(contactPrefix.length);
-            delete dataJSON[name];
-            res.end('success');
-        });
+        deleteContact(req, res);
     } else if (req.url.startsWith(contactPrefix) && req.method === 'POST') {
-        readbody(req, body => {
-            let contact = JSON.parse(body);
-            let dataJSON = JSON.stringify(contact)
-            writeFile('hobbit.json', dataJSON).then(() => {
-                console.log('success')
-            });
-            res.end('created contact');
-        });
+        postContact(req, res);
     } else if (req.url.startsWith(contactPrefix) && req.method === 'PUT') {
-        readbody(req, body => {
-            let contact = JSON.parse(body);
-            res.end('created contact');
-        });
+        putContact(req, res);
     }
 });
 
